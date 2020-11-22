f247172ab05f584d6fb72a6bbec113de622b38b4
于文静 <fbiwenjingyu@163.com>
2020-11-22 16:38:18 +0800

    修改统计方法

--- src/com/ywj/utils/DataBaseUtils.java
+++ src/com/ywj/utils/DataBaseUtils.java
@@ -1,8 +1,14 @@
 package com.ywj.utils;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ywj.model.DataBase;
+import com.ywj.model.DataSourceConnectDBVO;
 
 public class DataBaseUtils {
 	private DataBase database;
@@ -18,20 +24,61 @@ public class DataBaseUtils {
 		this.database = database;
 	}
 
+//	public long getDataNums() throws Exception{
+//		long nums = 0;
+//		String sqlStr = "select sum(t.table_rows) from (select table_name,table_rows from information_schema.tables where " + 
+//				"TABLE_SCHEMA = '" + this.getDataBaseName() + "' order by table_rows desc) as t";
+//		String url = "jdbc:mysql://" + getIpaddress() + ":" + getPort() + "/" + getDataBaseName() +"?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
+//		String username = getUserName();
+//		String password = getPassword();
+//		Connection con = JdbcUtils.getConn(url, username, password);
+//		java.sql.Statement statement = con.createStatement();
+//		ResultSet rs = statement.executeQuery(sqlStr);//执行查询语句，接收结果集
+//		if(rs.next()) {
+//			nums = rs.getLong(1);
+//		}
+//		return nums;
+//	}
+	
 	public long getDataNums() throws Exception{
 		long nums = 0;
-		String sqlStr = "select sum(t.table_rows) from (select table_name,table_rows from information_schema.tables where " + 
-				"TABLE_SCHEMA = '" + this.getDataBaseName() + "' order by table_rows desc) as t";
+		Connection con = getMysqlConnection();
+		List<String> tableNames = getMysqlTableNames();
+		
+		for(String tableName : tableNames) {
+			String countSql = "select count(*) from " +  "`" + tableName + "`";
+			PreparedStatement prepareStatement = con.prepareStatement(countSql);
+			ResultSet rs = prepareStatement.executeQuery();
+			if(rs.next()) {
+				long num = rs.getLong(1);
+				System.err.println("tableName = " + tableName + " table count = " + num);
+				nums += num;
+			}
+			prepareStatement.close();
+			rs.close();
+		}
+		con.close();
+		return nums;
+	}
+	
+	public List<String> getMysqlTableNames() throws Exception {
+		List<String> names = new ArrayList<String>();
+		Connection con = getMysqlConnection();
+		DatabaseMetaData metaData = con.getMetaData();
+		ResultSet rs = metaData.getTables(getDataBaseName(), "", null, null);
+		while(rs.next()) {
+			names.add(rs.getString(3));  
+		}
+		con.close();
+		return names;
+	}
+
+	private Connection getMysqlConnection() {
 		String url = "jdbc:mysql://" + getIpaddress() + ":" + getPort() + "/" + getDataBaseName() +"?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 		String username = getUserName();
 		String password = getPassword();
 		Connection con = JdbcUtils.getConn(url, username, password);
-		java.sql.Statement statement = con.createStatement();
-		ResultSet rs = statement.executeQuery(sqlStr);//执行查询语句，接收结果集
-		if(rs.next()) {
-			nums = rs.getLong(1);
-		}
-		return nums;
+		return con;
 	}
 	
 	private String getUserName() {
@@ -57,6 +104,8 @@ public class DataBaseUtils {
 		}
 		return databasename;
 	}
+	
+	
 	
 	
 
